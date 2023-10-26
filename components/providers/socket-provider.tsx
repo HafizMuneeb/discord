ad54446f05151199cee844380462c3bebabb7b7d
Muneeb <muneebafzal381a@gmail.com>
2023-10-26 18:41:06 +0500

    adding video call funtionality

--- components/providers/socket-provider.tsx
+++ components/providers/socket-provider.tsx
@@ -6,7 +6,7 @@ import {
   useEffect,
   useState
 } from "react";
-// import { io as ClientIO } from "socket.io-client";
+import { io as ClientIO } from "socket.io-client";
 
 type SocketContextType = {
   socket: any | null;
@@ -30,26 +30,26 @@ export const SocketProvider = ({
   const [socket, setSocket] = useState(null);
   const [isConnected, setIsConnected] = useState(false);
 
-//   useEffect(() => {
-//     const socketInstance = new (ClientIO as any)(process.env.NEXT_PUBLIC_SITE_URL!, {
-//       path: "/api/socket/io",
-//       addTrailingSlash: false,
-//     });
+  useEffect(() => {
+    const socketInstance = new (ClientIO as any)(process.env.NEXT_PUBLIC_SITE_URL!, {
+      path: "/api/socket/io",
+      addTrailingSlash: false,
+    });
 
-//     socketInstance.on("connect", () => {
-//       setIsConnected(true);
-//     });
+    socketInstance.on("connect", () => {
+      setIsConnected(true);
+    });
 
-//     socketInstance.on("disconnect", () => {
-//       setIsConnected(false);
-//     });
+    socketInstance.on("disconnect", () => {
+      setIsConnected(false);
+    });
 
-//     setSocket(socketInstance);
+    setSocket(socketInstance);
 
-//     return () => {
-//       socketInstance.disconnect();
-//     }
-//   }, []);
+    return () => {
+      socketInstance.disconnect();
+    }
+  }, []);
 
   return (
     <SocketContext.Provider value={{ socket, isConnected }}>
